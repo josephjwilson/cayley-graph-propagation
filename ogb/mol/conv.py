e6fd1febcf84e3a200522274d839cf3aff1d5cc9
JJ Wilson <jjwilson_@outlook.com>
2023-11-07 18:59:57 +0000

    Code updated for to implement CGP (EGP with virtual nodes). I don't normally added so many comments!

--- ogb/mol/conv.py
+++ ogb/mol/conv.py
@@ -63,16 +63,37 @@ def build_cayley_bank():
 
     return ret_edges
 
+# Here is where the changes to CGP start!
+# Note: this is extremely rough, hence I would probably refactor it a lot to split the concerns between responsibilities
+# This would be also nice to think about if we ever wanted to open-source our code :)
 def batched_augment_cayley(num_graphs, batch, cayley_bank):
     node_lims = np.zeros(num_graphs)
     mappings = [[] for _ in range(num_graphs)]
+    fake_mappings = [[] for _ in range(num_graphs)]
+
     for i in range(len(batch)):
-      node_lims[batch[i]] += 1
-      mappings[batch[i]].append(i)
+        node_lims[batch[i]] += 1
+        mappings[batch[i]].append(i)
+        fake_mappings[batch[i]].append(i)
 
     senders = []
     receivers = []
-    degs = np.zeros(len(batch))
+
+    # I probably should change th word fake, however this is the 'virtual nodes'
+    fake_senders = []
+    fake_receivers = []
+
+    # Track the original index
+    og_index = len(batch)
+
+    # This is a bad name, this basically keeps track of the index of the new upper node.
+    start_index = len(batch)
+
+    # The general idea is that it handles the input graph as always up to |V|
+    # Outside the region of |V| are where the new virtual nodes begin
+    # I.e. Say the nodes go up to 100 for the entire batch
+    # A virtual node may be between 101-110 for the first graph in the batch
+    # Then the second batch may between 110-120 etc.
 
     for g in range(num_graphs):
         p = 2
@@ -87,22 +108,49 @@ def batched_augment_cayley(num_graphs, batch, cayley_bank):
         _p, edge_pack = cayley_bank[chosen_i]
         assert p == _p
 
+        # Increment add extend the nodes to fill in the position of |V| > based on the prior upper limit
+        if sz > int(node_lims[g]):
+            missing_nodes = list(range(start_index, start_index + int(sz - node_lims[g])))
+            fake_mappings[g].extend(missing_nodes)
+            start_index += len(missing_nodes)
+
+        r_mappings = mappings[g]
+        f_mappings = fake_mappings[g]
+
+        assert sz == len(f_mappings)
+        
+        # Here is the simple change to say do I align with the input graph or am I out of the range
+        # If I'm out of the range - then I'm a virtual node
+        # Note* I have a newer version that handles the input graph being offset
+        # I.e. it does not align with node zero
         for v, w in zip(*edge_pack):
             if v < node_lims[g] and w < node_lims[g]:
-              senders.append(mappings[g][v])
-              receivers.append(mappings[g][w])
-              degs[mappings[g][w]] += 1
+                senders.append(r_mappings[v])
+                receivers.append(r_mappings[w])
+            else:
+                fake_senders.append(f_mappings[v])
+                fake_receivers.append(f_mappings[w])
 
-    for i in range(len(batch)):
+    self_indexes_senders = []
+    self_indexes_recievers = []
+    for i in range(og_index, start_index):
+        self_indexes_senders.append(i)
+        self_indexes_recievers.append(i)
+
+    senders.extend(fake_senders)
+    receivers.extend(fake_receivers)
+
+    # Add all  the new self edges
+    for i in range(start_index):
         senders.append(i)
         receivers.append(i)
-        degs[i] += 1
 
     edge_attr = []
     for i in range(len(senders)):
         edge_attr.append([0, 0, 0])  # Replace this with whatever edge features would be expected in your dataset :)
 
-    return [senders, receivers], edge_attr
+    # This output is a bit meh, and could be streamlined
+    return [senders, receivers], edge_attr, start_index, [self_indexes_senders, self_indexes_recievers]
 
 ### GIN convolution along the graph structure
 class GINConv(MessagePassing):
@@ -175,21 +223,51 @@ class GNN_node(torch.nn.Module):
         num_graphs = batched_data.num_graphs
         num_nodes = x.shape[0]
 
-        # Creates the Cayley graph based on the input graph
-        cayley_g, cayley_attr = batched_augment_cayley(num_graphs, batch, self.cayley_bank)
+        # This is similar to before, but with more outputs
+
+        # self_edge_index are just the self connections, outside of the graph node range.
+        # I.e. if it ends at 100, then [101, 101]...
+        # From the batched Cayley graph this is outside of the range
+        cayley_g, cayley_attr, max_node, self_edge_indexes = batched_augment_cayley(num_graphs, batch, self.cayley_bank)
         cayley_g = torch.LongTensor(cayley_g).cuda()
         cayley_attr = torch.LongTensor(cayley_attr).cuda()
+
+        # New self edges for dummy nodes
+        self_edge_indexes = torch.LongTensor(self_edge_indexes).cuda()
+        # edges indexes to be be used that include real edges for the graph + self edges for dummy nodes
+        new_edge_index = torch.cat((edge_index, self_edge_indexes), dim=1).cuda()
 
         ### computing input node embedding
 
         h_list = [self.atom_encoder(x)]
+
+        # Initialise them at layer 0 <- I just took the average nodes at this point
+        h_list_zero = h_list[0]
+
+        # Example of different strategies to initialise the dummy nodes
+        # Create the dummy nodes, expanding on layer[0] average
+        # average_real_nodes = torch.mean(h_list[0], dim=0).to(device)
+        # dummy_nodes = torch.randn(max_node - num_nodes, self.emb_dim).to(device)
+
+        # I have changed this to -1, 0
+        dummy_nodes = torch.zeros(max_node - num_nodes, self.emb_dim).cuda()
+
+        # Expand the dummy edge attr to match new_edge_index shape, of course we're just adding [0, 0, 0]
+        # Set the first nodes to what they were + [0, 0, 0] for everything else
+        dummy_edge_attr = torch.zeros(new_edge_index.shape[1], 3, dtype=torch.int64).cuda()
+        dummy_edge_attr[:edge_attr.shape[0], :] = edge_attr
+
+        # Expand the first layer to include dummy_nodes
+        h_list[0] = torch.cat((h_list[0], dummy_nodes), dim=0)
+
+        # The loop will now be larger than |V| containing the virtual nodes
         for layer in range(self.num_layer):
             
             # Alternate between Cayley graph and input graph
             if layer % 2 == 1:
                 h = self.convs[layer](h_list[layer], cayley_g, cayley_attr)
             else:
-                h = self.convs[layer](h_list[layer], edge_index, edge_attr)
+                h = self.convs[layer](h_list[layer], new_edge_index, dummy_edge_attr)
 
             h = self.batch_norms[layer](h)
 
@@ -211,6 +289,9 @@ class GNN_node(torch.nn.Module):
             node_representation = 0
             for layer in range(self.num_layer + 1):
                 node_representation += h_list[layer]
+
+        # Truncate back to the original input graph size for downstream usage
+        node_representation = h_list[-1][:num_nodes]
 
         return node_representation
 

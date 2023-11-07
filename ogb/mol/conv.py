4ba33fcdababe1de4814202c693f8bd6e20ed9a6
JJ Wilson <jjwilson_@outlook.com>
2023-11-07 18:47:01 +0000

    Updated the GNN node to implement EGP

--- ogb/mol/conv.py
+++ ogb/mol/conv.py
@@ -6,6 +6,103 @@ from ogb.graphproppred.mol_encoder import AtomEncoder,BondEncoder
 from torch_geometric.utils import degree
 
 import math
+import numpy as np
+from collections import deque
+
+_CAYLEY_BOUNDS = [
+    (6, 2),
+    (24, 3),
+    (120, 5),
+    (336, 7),
+    (1320, 11),
+    (2184, 13),
+    (4896, 17),
+    (6840, 19),
+    (12144, 23),
+    (24360, 29),
+    (29760, 31),
+    (50616, 37),
+]
+
+def build_cayley_bank():
+    ret_edges = []
+
+    for _, p in _CAYLEY_BOUNDS:
+        generators = np.array([
+            [[1, 1], [0, 1]],
+            [[1, p-1], [0, 1]],
+            [[1, 0], [1, 1]],
+            [[1, 0], [p-1, 1]]])
+        ind = 1
+
+        queue = deque([np.array([[1, 0], [0, 1]])])
+        nodes = {(1, 0, 0, 1): 0}
+
+        senders = []
+        receivers = []
+
+        while queue:
+            x = queue.pop()
+            x_flat = (x[0][0], x[0][1], x[1][0], x[1][1])
+            assert x_flat in nodes
+            ind_x = nodes[x_flat]
+            for i in range(4):
+                tx = np.matmul(x, generators[i])
+                tx = np.mod(tx, p)
+                tx_flat = (tx[0][0], tx[0][1], tx[1][0], tx[1][1])
+                if tx_flat not in nodes:
+                    nodes[tx_flat] = ind
+                    ind += 1
+                    queue.append(tx)
+                ind_tx = nodes[tx_flat]
+
+                senders.append(ind_x)
+                receivers.append(ind_tx)
+
+        ret_edges.append((p, [senders, receivers]))
+
+    return ret_edges
+
+def batched_augment_cayley(num_graphs, batch, cayley_bank):
+    node_lims = np.zeros(num_graphs)
+    mappings = [[] for _ in range(num_graphs)]
+    for i in range(len(batch)):
+      node_lims[batch[i]] += 1
+      mappings[batch[i]].append(i)
+
+    senders = []
+    receivers = []
+    degs = np.zeros(len(batch))
+
+    for g in range(num_graphs):
+        p = 2
+        chosen_i = -1
+        for i in range(len(_CAYLEY_BOUNDS)):
+            sz, p = _CAYLEY_BOUNDS[i]
+            if sz >= node_lims[g]:
+                chosen_i = i
+                break
+
+        assert chosen_i >= 0
+        _p, edge_pack = cayley_bank[chosen_i]
+        assert p == _p
+
+        for v, w in zip(*edge_pack):
+            if v < node_lims[g] and w < node_lims[g]:
+              senders.append(mappings[g][v])
+              receivers.append(mappings[g][w])
+              degs[mappings[g][w]] += 1
+
+    for i in range(len(batch)):
+        senders.append(i)
+        receivers.append(i)
+        degs[i] += 1
+
+    edge_attr = []
+    for i in range(len(senders)):
+        edge_attr.append([0, 0, 0])  # Replace this with whatever edge features would be expected in your dataset :)
+
+    return [senders, receivers], edge_attr
 
 ### GIN convolution along the graph structure
 class GINConv(MessagePassing):
@@ -58,6 +155,8 @@ class GNN_node(torch.nn.Module):
 
         self.atom_encoder = AtomEncoder(emb_dim)
 
+        self.cayley_bank = build_cayley_bank()
+
         ###List of GNNs
         self.convs = torch.nn.ModuleList()
         self.batch_norms = torch.nn.ModuleList()
@@ -73,12 +172,25 @@ class GNN_node(torch.nn.Module):
     def forward(self, batched_data):
         x, edge_index, edge_attr, batch = batched_data.x, batched_data.edge_index, batched_data.edge_attr, batched_data.batch
 
+        num_graphs = batched_data.num_graphs
+        num_nodes = x.shape[0]
+
+        # Creates the Cayley graph based on the input graph
+        cayley_g, cayley_attr = batched_augment_cayley(num_graphs, batch, self.cayley_bank)
+        cayley_g = torch.LongTensor(cayley_g).cuda()
+        cayley_attr = torch.LongTensor(cayley_attr).cuda()
+
         ### computing input node embedding
 
         h_list = [self.atom_encoder(x)]
         for layer in range(self.num_layer):
+            
+            # Alternate between Cayley graph and input graph
+            if layer % 2 == 1:
+                h = self.convs[layer](h_list[layer], cayley_g, cayley_attr)
+            else:
+                h = self.convs[layer](h_list[layer], edge_index, edge_attr)
 
-            h = self.convs[layer](h_list[layer], edge_index, edge_attr)
             h = self.batch_norms[layer](h)
 
             if layer == self.num_layer - 1:
